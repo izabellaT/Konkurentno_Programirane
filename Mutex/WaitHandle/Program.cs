eadd1f8d74ba3660cda14f328ef6134dc2c91bf0
izabellaT <izabellaT>
2023-04-05 15:42:21 +0300

    ryfgedok

---
+++ Mutex/WaitHandle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexZad
+{
+    class Program
+    {
+      
+            const int THREAD_COUNT = 5;
+            static void Main(string[] args)
+            {
+                Mutex commonMutex = new Mutex();
+                Thread[] demoThreads = new Thread[THREAD_COUNT];
+                for (int i = 0; i <THREAD_COUNT; i++)
+{
+                MutexThread mutexThread = new MutexThread(commonMutex);
+                demoThreads[i] = new Thread(
+                new ThreadStart(mutexThread.PerformSomeTask));
+                demoThreads[i].Start();
+            }
+            foreach (Thread thread in demoThreads)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("Main Thread Exist");
+        }
+    }
+    class MutexThread
+    {
+        Mutex mMutex;
+        public MutexThread(Mutex aMutex)
+        {
+            mMutex = aMutex;
+        }
+        public void PerformSomeTask()
+        {
+
+            mMutex.WaitOne();
+            Thread.Sleep(200);
+            Console.WriteLine("\nStarting some job...");
+            for (int i = 0; i<10; i++)
+{
+                Thread.Sleep(100);
+                Console.Write("|");
+            }
+            Console.WriteLine("\nJob finished.");
+            mMutex.ReleaseMutex();
+        }
+    }
+}
